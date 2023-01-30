7e698f4b58fd970b1cad8329dd7af690620cd727
nyrH-lewaP <166269@student.uwm.edu.pl>
2023-01-30 09:04:09 +0100

    iwo

--- E1-166324/zadanie1/Professor.java
+++ E1-166324/zadanie1/Professor.java
@@ -1,9 +1,24 @@
 package zadanie1;
 
-public class Professor extends Student {
+import java.util.Date;
+
+public class Professor extends Student implements Cloneable {
     public Professor(String name, int id) {
         super(name, id);
+        this.id = id;
+        this.nazwiko = name;
     }
 
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        try{
+            Professor cloned = (Professor) super.clone();
+            return cloned;
+        }catch (CloneNotSupportedException e){
+            return new CloneNotSupportedException();
+        }
+    }
+    private int id;
+    private String nazwiko;
 
 }
--- E1-166324/zadanie1/StudentDemo.java
+++ E1-166324/zadanie1/StudentDemo.java
@@ -11,17 +11,17 @@ public class StudentDemo {
 
         Named n = s;
 
-        System.out.println(n.getName());
-        Professor d = s.clone();
-        System.out.println(d.getName());
-        System.out.println(d);
+//        System.out.println(n.getName());
+//        Professor d = (Professor) s.clone();
+//        System.out.println(d.getName());
+//        System.out.println(d);
 
         ArrayList<Professor> studenci = new ArrayList<>();
         studenci.add(new Professor("Zenon", 1729));
         studenci.add(new Professor("Andrzej", 1729));
         studenci.add(new Professor("Kamil", 1729));
 
-        studenci.sort(new NamesComparator());
+        studenci.sort(null);
 
         System.out.println();
         for (Professor e : studenci) {
